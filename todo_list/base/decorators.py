61c01433c41a9462e5e338e9e7dac21e6148ea7f
Khanh Toan <laikhanhtoanb10@gmail.com>
2022-07-24 21:54:42 +0700

    change files and fixed bugs

---
+++ todo_list/base/decorators.py
@@ -0,0 +1,12 @@
+import django
+from django.shortcuts import redirect
+from django.http import HttpResponse
+
+
+def unauthenticated_user(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        
+        return view_func(request, *args, **kwargs)  
+    return wrapper_func
--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 from .models import Task
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from .filters import TaskFilter
+from django.contrib.auth.decorators import login_required
+from .decorators import unauthenticated_user
 
 # Create your views here.
 class CustomLoginView(LoginView):
@@ -17,19 +22,56 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('tasks')
 
-class TaskList(LoginRequiredMixin, ListView):
-    model = Task
-    context_object_name = 'tasks'
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
 
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+class TaskList(LoginRequiredMixin, ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        temp1 = context['tasks'].filter(user=self.request.user)
+        temp2 = context['tasks'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        temp3 = None
+        if search_input:
+            temp3 = context['tasks'].filter(
+                title__contains=search_input)
+
+        temp4 = search_input
+        temp5 = context['tasks']
+        context = {'temp1':temp1,'temp2':temp2, 'temp4':temp4, 'temp5':temp5}
+        return context 
     
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
+
+    def form_invalid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
@@ -40,3 +82,19 @@ class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
+
+@unauthenticated_user
+def loginPage(request):
+    if request.method =='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('tasks')
+        else :
+            messages.info(request,'User or password is incorrect')
+    context ={}
+    return render(request,'base/login.html',context)
+
